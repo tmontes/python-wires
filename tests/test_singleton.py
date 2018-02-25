4ba9d12f37dcb71a52eb58191a3a034ec72c198f
Tiago Montes <tiago.montes@gmail.com>
2018-02-25 13:06:22 +0000

    Implemented per-wiring args and kwargs. Tested.

--- tests/test_singleton.py
+++ tests/test_singleton.py
@@ -62,30 +62,35 @@ class TestWiresAPI(unittest.TestCase):
 
 
     @staticmethod
-    def test_wiring_callable_works():
+    def _test_callable():
+        pass
+
+
+    def test_wiring_callable_works(self):
         """
         Wiring a callable works.
         """
-        test_callable = lambda: None
-
         # Calling test_callble works (for coverage completion's sake).
-        test_callable()
+        self._test_callable()
 
-        wire.this.calls_to(test_callable)
+        wire.this.calls_to(self._test_callable)
+        self.addCleanup(self._unwire_test_callable)
 
 
-    @staticmethod
-    def test_wiring_unwiring_works():
+    def _unwire_test_callable(self):
+
+        unwire.this.calls_to(self._test_callable)
+
+
+    def test_wiring_unwiring_works(self):
         """
         Wiring and then unwiring to the same callable works.
         """
-        test_callable = lambda: None
-
         # Calling test_callble works (for coverage completion's sake).
-        test_callable()
+        self._test_callable()
 
-        wire.this.calls_to(test_callable)
-        unwire.this.calls_to(test_callable)
+        wire.this.calls_to(self._test_callable)
+        unwire.this.calls_to(self._test_callable)
 
 
     def test_unwiring_unknown_callable_raises_value_error(self):
@@ -94,13 +99,8 @@ class TestWiresAPI(unittest.TestCase):
         The exception argument (message):
         - Starts with "unknown function ".
         """
-        test_callable = lambda: None
-
-        # Calling test_callble works (for coverage completion's sake).
-        test_callable()
-
         with self.assertRaises(ValueError) as cm:
-            unwire.this.calls_to(test_callable)
+            unwire.this.calls_to(self._test_callable)
 
         exception_args = cm.exception.args
         self.assertEqual(len(exception_args), 1)
@@ -366,6 +366,184 @@ class TestWiresCalleeFailures(unittest.TestCase):
 
         # We get no stderr output.
         self.assertEqual(self.stderr.getvalue(), '', 'stderr output')
+
+
+
+class ArgPassingMixin(helpers.CallTrackerAssertMixin):
+
+    """
+    Argument passing utilization tests for the Wires singleton.
+    """
+
+    wire1_args = ()
+    wire1_kwargs = {}
+
+    wire2_args = ()
+    wire2_kwargs = {}
+
+    call_args = ()
+    call_kwargs = {}
+
+
+    def test_wire_call_args(self):
+        """
+        Wires the `this` call and calls it.
+        Checks the wired callable was called once with the correct arguments.
+        """
+        tracker = helpers.CallTracker()
+
+        wire.this.calls_to(tracker, *self.wire1_args, **self.wire1_kwargs)
+        wire.this(*self.call_args, **self.call_kwargs)
+        self.addCleanup(self._unwire, tracker)
+
+        self.assertEqual(tracker.call_count, 1, 'call count mismatch')
+        self.assertEqual(tracker.call_args, [
+            (self.expected_call_args, self.expected_call_kwargs),
+        ], 'call argument mismatch')
+
+
+    def test_double_wire_call_args(self):
+        """
+        Wires the `this` call and calls it.
+        Checks the wired callable was called once with the correct arguments.
+        """
+        tracker = helpers.CallTracker()
+
+        wire.this.calls_to(tracker, *self.wire1_args, **self.wire1_kwargs)
+        wire.this.calls_to(tracker, *self.wire2_args, **self.wire2_kwargs)
+        wire.this(*self.call_args, **self.call_kwargs)
+        self.addCleanup(self._unwire, tracker)
+        self.addCleanup(self._unwire, tracker)
+
+        self.assertEqual(tracker.call_count, 2, 'call count mismatch')
+        self.assertEqual(tracker.call_args, [
+            (self.expected_call_args, self.expected_call_kwargs),
+            (self.expected_2nd_call_args, self.expected_2nd_call_kwargs),
+        ], 'call argument mismatch')
+
+
+    def _unwire(self, tracker):
+
+        unwire.this.calls_to(tracker)
+
+
+
+class TestWiresNoArgPassing(ArgPassingMixin, unittest.TestCase):
+
+    expected_call_args = ()
+    expected_call_kwargs = {}
+
+    expected_2nd_call_args = ()
+    expected_2nd_call_kwargs = {}
+
+
+
+class TestWiresWireArgPassing(ArgPassingMixin, unittest.TestCase):
+
+    wire1_args = (1, 2, 3)
+
+    expected_call_args = (1, 2, 3)
+    expected_call_kwargs = {}
+
+    expected_2nd_call_args = ()
+    expected_2nd_call_kwargs = {}
+
+
+
+class TestWiresWireKwargPassing(ArgPassingMixin, unittest.TestCase):
+
+    wire1_kwargs = dict(a='a', b='b')
+
+    expected_call_args = ()
+    expected_call_kwargs = dict(a='a', b='b')
+
+    expected_2nd_call_args = ()
+    expected_2nd_call_kwargs = {}
+
+
+
+class TestWiresWireArgKwargPassing(ArgPassingMixin, unittest.TestCase):
+
+    wire1_args = (1, 2, 3)
+    wire1_kwargs = dict(a='a', b='b')
+
+    expected_call_args = (1, 2, 3)
+    expected_call_kwargs = dict(a='a', b='b')
+
+    expected_2nd_call_args = ()
+    expected_2nd_call_kwargs = {}
+
+
+
+class TestWiresCallArgPassing(ArgPassingMixin, unittest.TestCase):
+
+    call_args = (1, 2, 3)
+
+    expected_call_args = (1, 2, 3)
+    expected_call_kwargs = {}
+
+    expected_2nd_call_args = (1, 2, 3)
+    expected_2nd_call_kwargs = {}
+
+
+
+class TestWiresCallKwargPassing(ArgPassingMixin, unittest.TestCase):
+
+    call_kwargs = dict(a='a', b='b')
+
+    expected_call_args = ()
+    expected_call_kwargs = dict(a='a', b='b')
+
+    expected_2nd_call_args = ()
+    expected_2nd_call_kwargs = dict(a='a', b='b')
+
+
+
+class TestWiresCallArgKwargPassing(ArgPassingMixin, unittest.TestCase):
+
+    call_args = (1, 2, 3)
+    call_kwargs = dict(a='a', b='b')
+
+    expected_call_args = (1, 2, 3)
+    expected_call_kwargs = dict(a='a', b='b')
+
+    expected_2nd_call_args = (1, 2, 3)
+    expected_2nd_call_kwargs = dict(a='a', b='b')
+
+
+
+class TestWiresFullPassing(ArgPassingMixin, unittest.TestCase):
+
+    wire1_args = (1, 2, 3)
+    wire1_kwargs = dict(a='a', b='b')
+
+    call_args = (4, 5, 6)
+    call_kwargs = dict(c='c', d='d')
+
+    expected_call_args = (1, 2, 3, 4, 5, 6)
+    expected_call_kwargs = dict(a='a', b='b', c='c', d='d')
+
+    expected_2nd_call_args = (4, 5, 6)
+    expected_2nd_call_kwargs = dict(c='c', d='d')
+
+
+
+class TestWiresDoubleFullPassing(ArgPassingMixin, unittest.TestCase):
+
+    wire1_args = (1, 2, 3)
+    wire1_kwargs = dict(a='a', b='b')
+
+    wire2_args = (4, 5, 6)
+    wire2_kwargs = dict(c='c', d='d')
+
+    call_args = (7, 8, 9)
+    call_kwargs = dict(e='e', f='f')
+
+    expected_call_args = (1, 2, 3, 7, 8, 9)
+    expected_call_kwargs = dict(a='a', b='b', e='e', f='f')
+
+    expected_2nd_call_args = (4, 5, 6, 7, 8, 9)
+    expected_2nd_call_kwargs = dict(c='c', d='d', e='e', f='f')
 
 
 # ----------------------------------------------------------------------------
