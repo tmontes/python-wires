734a2897acb9ca8000c835f8cc89b6e33488fcbb
Tiago Montes <tiago.montes@gmail.com>
2018-03-05 16:04:39 +0000

    More test docstrings.

--- tests/mixin_test_instance.py
+++ tests/mixin_test_instance.py
@@ -5,6 +5,10 @@
 # See LICENSE for deatils.
 # ----------------------------------------------------------------------------
 
+"""
+Wires instance test mixin.
+"""
+
 
 from __future__ import absolute_import
 
@@ -19,25 +23,33 @@ class TestWiresInstanceMixin(object):
     """
 
     def setUp(self):
-
+        """
+        Need a per-test instance: classes we mixin with must call this.
+        """
         self._w = Wiring()
 
 
     @property
     def w(self):
-
+        """
+        Per test mixin contract: self.w is a Wiring instance.
+        """
         return self._w
 
 
     @property
     def wire(self):
-
+        """
+        Per test mixin contract: self.wire wires callables.
+        """
         return self._w.wire
 
 
     @property
     def unwire(self):
-
+        """
+        Per test mixin contract: self.unwire unwires callabless.
+        """
         return self._w.unwire
 
 
