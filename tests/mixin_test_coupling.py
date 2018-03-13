82e2f450a7380c49f12d9e70802ed89ead45c0a4
Tiago Montes <tiago.montes@gmail.com>
2018-03-13 10:12:24 +0000

    Redesined call coupling tests: now declarative and dynamically generated.

--- tests/mixin_test_coupling.py
+++ tests/mixin_test_coupling.py
@@ -12,6 +12,8 @@ Caller/callee coupling test driver mixin.
 
 from __future__ import absolute_import
 
+from wires import Wiring
+
 from . import mixin_test_callables
 
 
@@ -19,7 +21,7 @@ from . import mixin_test_callables
 class WireAssertCouplingTestMixin(mixin_test_callables.TestCallablesMixin):
 
     """
-    Wiring and Assertion mixin for caller/callee call-time coupling tests.
+    Wiring and Assertion mixin for coupling tests.
     """
 
     def wire_returns_42_callable(self):
@@ -86,68 +88,274 @@ class WireAssertCouplingTestMixin(mixin_test_callables.TestCallablesMixin):
         self.assertEqual(exception_args[1], (self.THE_EXCEPTION, None))
 
 
+    TESTS = [
 
-class TestCallerCalleeCouplingMixin(WireAssertCouplingTestMixin):
+    # Default Wiring instantiation arguments.
+
+    {
+        'test-name': 'test_wire_returns_42_default_call',
+        'wiring-args': {},
+        'wire': wire_returns_42_callable,
+        'args-override': {},
+        'raises': None,
+        'assert': assert_result_wire_returns_42_callable,
+    }, {
+        'test-name': 'test_wire_raises_exception_default_call',
+        'wiring-args': {},
+        'wire': wire_raises_exeption_callable,
+        'args-override': {},
+        'raises': None,
+        'assert': assert_result_wire_raises_exeption_callable,
+    }, {
+        'test-name': 'test_wire_wire_wire_default_call',
+        'wiring-args': {},
+        'wire': wire_three_callables_2nd_one_failing,
+        'args-override': {},
+        'raises': None,
+        'assert': assert_result_wire_three_callables_2nd_one_failing,
+    }, {
+        'test-name': 'test_wire_returns_42_results_true_call',
+        'wiring-args': {},
+        'wire': wire_returns_42_callable,
+        'args-override': {"coupling": True},
+        'raises': None,
+        'assert': assert_result_wire_returns_42_callable,
+    }, {
+        'test-name': 'test_wire_raises_exception_results_true_call',
+        'wiring-args': {},
+        'wire': wire_raises_exeption_callable,
+        'args-override': {"coupling": True},
+        'raises': RuntimeError,
+        'assert': assert_failure_wire_raises_exeption_callable,
+    }, {
+        'test-name': 'test_wire_wire_wire_results_true_call',
+        'wiring-args': {},
+        'wire': wire_three_callables_2nd_one_failing,
+        'args-override': {"coupling": True},
+        'raises': RuntimeError,
+        'assert': assert_failure_wire_three_callables_2nd_one_failing,
+    }, {
+        'test-name': 'test_wire_returns_42_results_false_call',
+        'wiring-args': {},
+        'wire': wire_returns_42_callable,
+        'args-override': {"coupling": False},
+        'raises': None,
+        'assert': assert_result_wire_returns_42_callable,
+    }, {
+        'test-name': 'test_wire_raises_exception_results_false_call',
+        'wiring-args': {},
+        'wire': wire_raises_exeption_callable,
+        'args-override': {"coupling": False},
+        'raises': None,
+        'assert': assert_result_wire_raises_exeption_callable,
+    }, {
+        'test-name': 'test_wire_wire_wire_results_false_call',
+        'wiring-args': {},
+        'wire': wire_three_callables_2nd_one_failing,
+        'args-override': {"coupling": False},
+        'raises': None,
+        'assert': assert_result_wire_three_callables_2nd_one_failing,
+    },
+
+    # Wiring instantiation with coupling=False
+
+    {
+        'test-name': 'test_wire_returns_42_default_call',
+        'wiring-args': {"coupling": False},
+        'wire': wire_returns_42_callable,
+        'args-override': {},
+        'raises': None,
+        'assert': assert_result_wire_returns_42_callable,
+    }, {
+        'test-name': 'test_wire_raises_exception_default_call',
+        'wiring-args': {"coupling": False},
+        'wire': wire_raises_exeption_callable,
+        'args-override': {},
+        'raises': None,
+        'assert': assert_result_wire_raises_exeption_callable,
+    }, {
+        'test-name': 'test_wire_wire_wire_default_call',
+        'wiring-args': {"coupling": False},
+        'wire': wire_three_callables_2nd_one_failing,
+        'args-override': {},
+        'raises': None,
+        'assert': assert_result_wire_three_callables_2nd_one_failing,
+    }, {
+        'test-name': 'test_wire_returns_42_results_true_call',
+        'wiring-args': {"coupling": False},
+        'wire': wire_returns_42_callable,
+        'args-override': {"coupling": True},
+        'raises': None,
+        'assert': assert_result_wire_returns_42_callable,
+    }, {
+        'test-name': 'test_wire_raises_exception_results_true_call',
+        'wiring-args': {"coupling": False},
+        'wire': wire_raises_exeption_callable,
+        'args-override': {"coupling": True},
+        'raises': RuntimeError,
+        'assert': assert_failure_wire_raises_exeption_callable,
+    }, {
+        'test-name': 'test_wire_wire_wire_results_true_call',
+        'wiring-args': {"coupling": False},
+        'wire': wire_three_callables_2nd_one_failing,
+        'args-override': {"coupling": True},
+        'raises': RuntimeError,
+        'assert': assert_failure_wire_three_callables_2nd_one_failing,
+    }, {
+        'test-name': 'test_wire_returns_42_results_false_call',
+        'wiring-args': {"coupling": False},
+        'wire': wire_returns_42_callable,
+        'args-override': {"coupling": False},
+        'raises': None,
+        'assert': assert_result_wire_returns_42_callable,
+    }, {
+        'test-name': 'test_wire_raises_exception_results_false_call',
+        'wiring-args': {"coupling": False},
+        'wire': wire_raises_exeption_callable,
+        'args-override': {"coupling": False},
+        'raises': None,
+        'assert': assert_result_wire_raises_exeption_callable,
+    }, {
+        'test-name': 'test_wire_wire_wire_results_false_call',
+        'wiring-args': {"coupling": False},
+        'wire': wire_three_callables_2nd_one_failing,
+        'args-override': {"coupling": False},
+        'raises': None,
+        'assert': assert_result_wire_three_callables_2nd_one_failing,
+    },
+
+    # Wiring instantiation with coupling=True
+
+    {
+        'test-name': 'test_wire_returns_42_default_call',
+        'wiring-args': {"coupling": True},
+        'wire': wire_returns_42_callable,
+        'args-override': {},
+        'raises': None,
+        'assert': assert_result_wire_returns_42_callable,
+    }, {
+        'test-name': 'test_wire_raises_exception_default_call',
+        'wiring-args': {"coupling": True},
+        'wire': wire_raises_exeption_callable,
+        'args-override': {},
+        'raises': RuntimeError,
+        'assert': assert_failure_wire_raises_exeption_callable,
+    }, {
+        'test-name': 'test_wire_wire_wire_default_call',
+        'wiring-args': {"coupling": True},
+        'wire': wire_three_callables_2nd_one_failing,
+        'args-override': {},
+        'raises': RuntimeError,
+        'assert': assert_failure_wire_three_callables_2nd_one_failing,
+    }, {
+        'test-name': 'test_wire_returns_42_results_true_call',
+        'wiring-args': {"coupling": True},
+        'wire': wire_returns_42_callable,
+        'args-override': {"coupling": True},
+        'raises': None,
+        'assert': assert_result_wire_returns_42_callable,
+    }, {
+        'test-name': 'test_wire_raises_exception_results_true_call',
+        'wiring-args': {"coupling": True},
+        'wire': wire_raises_exeption_callable,
+        'args-override': {"coupling": True},
+        'raises': RuntimeError,
+        'assert': assert_failure_wire_raises_exeption_callable,
+    }, {
+        'test-name': 'test_wire_wire_wire_results_true_call',
+        'wiring-args': {"coupling": True},
+        'wire': wire_three_callables_2nd_one_failing,
+        'args-override': {"coupling": True},
+        'raises': RuntimeError,
+        'assert': assert_failure_wire_three_callables_2nd_one_failing,
+    }, {
+        'test-name': 'test_wire_returns_42_results_false_call',
+        'wiring-args': {"coupling": True},
+        'wire': wire_returns_42_callable,
+        'args-override': {"coupling": False},
+        'raises': None,
+        'assert': assert_result_wire_returns_42_callable,
+    }, {
+        'test-name': 'test_wire_raises_exception_results_false_call',
+        'wiring-args': {"coupling": True},
+        'wire': wire_raises_exeption_callable,
+        'args-override': {"coupling": False},
+        'raises': None,
+        'assert': assert_result_wire_raises_exeption_callable,
+    }, {
+        'test-name': 'test_wire_wire_wire_results_false_call',
+        'wiring-args': {"coupling": True},
+        'wire': wire_three_callables_2nd_one_failing,
+        'args-override': {"coupling": False},
+        'raises': None,
+        'assert': assert_result_wire_three_callables_2nd_one_failing,
+    }
+    
+    ]
+
+
+
+class TestCouplingMixin(WireAssertCouplingTestMixin):
 
     """
-    Drives Wires caller/callee coupling tests.
+    Drives Wires call coupling tests.
     """
 
-    def test_wire_default_decoupled_call(self):
-        """
-        Default uncoupled test: return a list of (<exception>, <result>), per-
-        callee tuple.
-        """
-        self.wire_returns_42_callable()
-        result = self.w.this()
-        self.assert_result_wire_returns_42_callable(result)
+    # Tests added via create_test_methods, below.
 
 
-    def test_wire_wire_wire_default_decoupled_call(self):
-        """
-        Default uncoupled test: return a list of (<exception>, <result>), per-
-        callee tuple.
-        """
-        self.wire_three_callables_2nd_one_failing()
-        result = self.w.this()
-        self.assert_result_wire_three_callables_2nd_one_failing(result)
+
+def _create_test_method(test_dict):
+
+    wiring_args = test_dict['wiring-args']
+    wire_callable = test_dict['wire']
+    args_override = test_dict['args-override']
+    raises = test_dict['raises']
+    assert_callable = test_dict['assert']
+
+    def test(self):
+        if wiring_args:
+            self.w = Wiring(**wiring_args)
+        wire_callable(self)
+        if raises:
+            with self.assertRaises(raises) as result:
+                self.w(**args_override).this()
+        else:
+            result = self.w(**args_override).this()
+        assert_callable(self, result)
+
+    return test
 
 
-    def test_wire_force_coupled_call(self):
-        """
-        Wire a callable, call it forcing coupling.
-        """
-        self.wire_returns_42_callable()
-        result = self.w(coupling=True).this()
-        self.assert_result_wire_returns_42_callable(result)
+
+def create_test_methods(test_class, wiring_args=None):
+    """
+    Creates test methods on `test_class` having a `TESTS` test description list.
+    Test methods with be created if:
+    - The test description dict 'wiring-args' key matches `wiring_args`.
+    """
+    for test_dict in test_class.TESTS:
+        test_wiring_args = test_dict['wiring-args']
+        if wiring_args is None:
+            if test_wiring_args:
+                # skip test with wiring args generation
+                continue
+        else:
+            skip = False
+            for arg, value in wiring_args.items():
+                if test_wiring_args.get(arg) != value:
+                    # skip test with differet wiring args generation
+                    skip = True
+                    break
+            if skip:
+                continue
+        test_method = _create_test_method(test_dict)
+        test_method_name = test_dict['test-name']
+        setattr(test_class, test_method_name, test_method)
 
 
-    def test_wire_wire_wire_coupled_call(self):
-        """
-        Wire a callable three times: the second one raises an exception.
-        """
-        self.wire_three_callables_2nd_one_failing()
-        with self.assertRaises(RuntimeError) as cm:
-            self.w(coupling=True).this()
-        self.assert_failure_wire_three_callables_2nd_one_failing(cm)
 
-
-    def test_wire_force_decoupled_call(self):
-        """
-        Wire a callable, call it with no coupling.
-        """
-        self.wire_returns_42_callable()
-        result = self.w(coupling=False).this()
-        self.assert_result_wire_returns_42_callable(result)
-
-
-    def test_wire_wire_wire_decoupled_call(self):
-        """
-        Wire a callable three times: the second one raises an exception.
-        """
-        self.wire_three_callables_2nd_one_failing()
-        result = self.w(coupling=False).this()
-        self.assert_result_wire_three_callables_2nd_one_failing(result)
+create_test_methods(TestCouplingMixin)
 
 
 # ----------------------------------------------------------------------------
